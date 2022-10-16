77e34a0297d9a5444a68566ca4faafb6295e57ef
Azamat <azamatnamialy@gmail.com>
2022-10-16 15:25:09 +0600

    add quasar ui framework

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Quasar } from "quasar";
+import quasarUserOptions from "./quasar-user-options";
+import "quasar/dist/quasar.sass";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(Quasar, quasarUserOptions)
+  .use(store)
+  .use(router)
+  .mount("#app");
